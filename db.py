e5484d2aed6a3cae036c1962287952eb60abea77
Farahin Affendy <nurfarahin@tmrnd.com.my>
2025-08-17 02:29:27 +0800

    disable ssl

--- db.py
+++ db.py
@@ -2,4 +2,4 @@ password = "ptNetFCcvTtCmefnLqzZkjXyctJWLxhi"
 host = "switchyard.proxy.rlwy.net:59085"
 db_name = "edupointx"
 
-DB_URL = "mysql+pymysql://root:ptNetFCcvTtCmefnLqzZkjXyctJWLxhi@switchyard.proxy.rlwy.net:59085/edupointx"
+DB_URL = "mysql+pymysql://root:ptNetFCcvTtCmefnLqzZkjXyctJWLxhi@switchyard.proxy.rlwy.net:59085/edupointx?ssl_disabled=false"
