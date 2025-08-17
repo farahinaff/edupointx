c19608d02ffb432dc9cf2460774d21bed4b196e3
Farahin Affendy <nurfarahin@tmrnd.com.my>
2025-08-17 13:55:22 +0800

    solve conflicts

--- EDUPOINTX/modules/db.py
+++ EDUPOINTX/modules/db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 from sqlalchemy import create_engine
 
@@ -6,12 +5,3 @@ cfg = st.secrets["db"]
 
 DB_URL = f"mysql+pymysql://{cfg.user}:{cfg.password}@{cfg.host}:{cfg.port}/{cfg.database}?ssl_disabled=false"
 engine = create_engine(DB_URL)
-=======
-import streamlit as st
-from sqlalchemy import create_engine
-
-cfg = st.secrets["db"]
-
-DB_URL = f"mysql+pymysql://{cfg.user}:{cfg.password}@{cfg.host}:{cfg.port}/{cfg.database}?ssl_disabled=false"
-engine = create_engine(DB_URL)
->>>>>>> 51725cf78ba6cea443ded2ef2e6dfd38880ba3c1
